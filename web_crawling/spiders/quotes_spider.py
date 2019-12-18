939c1071d1e1eae4ddea6ed2d6fe90a91379855e
XoanTran2811 <XoanTran2811@gmai.com>
2019-12-18 09:00:10 +0700

    author spider

--- web_crawling/spiders/quotes_spider.py
+++ web_crawling/spiders/quotes_spider.py
@@ -18,3 +18,26 @@ class QuotesSpider(scrapy.Spider):
         for a in response.css('li.next a'):
             yield response.follow(a, callback=self.parse)
 
+
+    class AuthorSpider(scrapy.Spider):
+        name = 'author'
+        start_urls = ['http://quotes.toscrape.com/']
+
+        def parse(self, response):
+            #follow links to author pages
+            for href in  response.css('.author + a::attr(href)'):
+                yield response.follow(href, self.parse_author)
+
+            # follow pagination link 
+            for href in response.css('li.next a::attr(href)'):
+                yield response.follow(href, self.parse)
+
+        def parse_author(self, response):
+            def extract_with_css(query):
+                return response.css(query).get(default='').strip()
+
+            yield {
+                'name':extract_with_css('h3.author-title::text'),
+                'birthdate': extract_with_css('.author-born-date::text'),
+                'bio': extract_with_css('.author-description::text'),
+            }    
