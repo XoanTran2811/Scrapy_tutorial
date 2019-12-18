6fc4eb19c581a63fb2eba57c8b785a7b471315a6
XoanTran2811 <XoanTran2811@gmai.com>
2019-12-18 09:06:17 +0700

    update author spider

--- web_crawling/spiders/quotes_spider.py
+++ web_crawling/spiders/quotes_spider.py
@@ -19,25 +19,25 @@ class QuotesSpider(scrapy.Spider):
             yield response.follow(a, callback=self.parse)
 
 
-    class AuthorSpider(scrapy.Spider):
-        name = 'author'
-        start_urls = ['http://quotes.toscrape.com/']
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+    start_urls = ['http://quotes.toscrape.com/']
 
-        def parse(self, response):
-            #follow links to author pages
-            for href in  response.css('.author + a::attr(href)'):
-                yield response.follow(href, self.parse_author)
+    def parse(self, response):
+        #follow links to author pages
+        for href in  response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
 
-            # follow pagination link 
-            for href in response.css('li.next a::attr(href)'):
-                yield response.follow(href, self.parse)
+        # follow pagination link 
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
 
-        def parse_author(self, response):
-            def extract_with_css(query):
-                return response.css(query).get(default='').strip()
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
 
-            yield {
-                'name':extract_with_css('h3.author-title::text'),
-                'birthdate': extract_with_css('.author-born-date::text'),
-                'bio': extract_with_css('.author-description::text'),
+        yield {
+            'name':extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
             }    
